1f76c439bf3042555b9c08d26f85f7408dfa932e
nithishkumar2303 <prof.nithishkumarj@gmail.com>
2026-01-21 22:18:05 +0530

    new files

--- app.py
+++ app.py
@@ -57,5 +57,5 @@ def create_app():
     return app
 
 if __name__ == '__main__':
-    app = create_app()
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
