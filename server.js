4c24c8788abcbb5a14a0efedfa77ff067892f2ab
nithishkumar2303 <prof.nithishkumarj@gmail.com>
2026-01-21 21:49:54 +0530

    ne files

--- server.js
+++ server.js
@@ -1,4 +1,11 @@
+const express = require("express");
+const app = express();
+
 const PORT = process.env.PORT || 8000;
+
+// middleware + routes here
+// app.use(...)
+
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-  });
+  console.log(`Server running on port ${PORT}`);
+});
